b563c01631f965d344ecefbb7c7d6b0c8759eca5
Clase <Clase@1A-PC16>
2018-01-04 18:38:13 -0500

    CalculadoraPromedio v2

--- CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
+++ CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
@@ -19,7 +19,29 @@ namespace CalculadoraNotaFinal
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			
 
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var c1 = Convert.ToInt32(textBox1.Text);
+			var c2 = Convert.ToInt32(textBox2.Text);
+			var c3 = Convert.ToInt32(textBox3.Text);
+			var c4 = Convert.ToInt32(textBox4.Text);
+
+			//var controlbajo = Math.Min(control1, control2, control3, control4);
+
+			var menorControl = new int[] { c1, c2, c3, c4 }.Min();
+
+			var promedioControles = (c1 + c2 + c3 + c4 - menorControl) / 3;
+
+			MessageBox.Show("El promedio de controles es : " + promedioControles);
 		}
 	}
 }
