dff3f7af884e5ee40c0b7249be2bd200ed2421a7
Clase <Clase@1A-PC16>
2018-01-04 19:30:11 -0500

    Nota final v3

--- CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
+++ CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
@@ -30,6 +30,8 @@ namespace CalculadoraNotaFinal
 
 		private void button1_Click(object sender, EventArgs e)
 		{
+
+			/////////promedio de controles
 			var c1 = Convert.ToInt32(textBox1.Text);
 			var c2 = Convert.ToInt32(textBox2.Text);
 			var c3 = Convert.ToInt32(textBox3.Text);
@@ -41,7 +43,53 @@ namespace CalculadoraNotaFinal
 
 			var promedioControles = (c1 + c2 + c3 + c4 - menorControl) / 3;
 
-			MessageBox.Show("El promedio de controles es : " + promedioControles);
+
+			label14.Text = "El promedio de controles es : " + promedioControles;
+
+			////////promedio de laboratorio
+			var l1 = Convert.ToInt32(textBox5.Text);
+			var l2 = Convert.ToInt32(textBox6.Text);
+			var l3 = Convert.ToInt32(textBox7.Text);
+			var l4 = Convert.ToInt32(textBox8.Text);
+
+			var promedioLaboratorio = (l1 + l2 + l3 + l4) / 4;
+
+			label15.Text = "El promedio de laboratorio:" + promedioLaboratorio;
+
+			///////promedio de evaluaciones
+
+			var W = Convert.ToInt32(textBox9.Text);
+			var promEva = (promedioControles + promedioLaboratorio + W) / 3;
+
+			label16.Text = "El promedio de evaluaciones:" + promEva;
+
+			///// promedio nota final
+
+			var par = Convert.ToInt32(textBox10.Text);
+			var fin = Convert.ToInt32(textBox10.Text);
+
+			var promNotaFinal = (2 * promEva + par + fin) / 4;
+			label17.Text = "Nota final:" + promNotaFinal;
+		}
+
+		private void label8_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label9_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox9_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label10_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
